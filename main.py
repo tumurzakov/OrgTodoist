7ce00573bcb937f2eb1867da4970f26d50665dec
Temir Umurzakov <tima@tima.work>
2016-11-27 23:48:49 +0600

    Save only if changed

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os, sys, inspect
+
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+sys.path.append(os.path.join(cmd_folder, "./dist/PyOrgMode"))
+sys.path.append(os.path.join(cmd_folder, "./dist/todoist-python"))
+
 from PyOrgMode import PyOrgMode
 import todoist
 import os.path
@@ -18,6 +23,8 @@ class OrgTodoist:
         self.__touch_org_file()
         self.org = PyOrgMode.OrgDataStructure()
         self.org.load_from_file(self.org_file)
+
+        self.changed = 0
 
     def __touch_org_file(self):
         try:
@@ -53,13 +60,19 @@ class OrgTodoist:
         self.todoist_items = response['items']
 
     def __process_scheduled(self, todo, item):
-        _due = self.__get_localtime(item['due_date_utc'])
+        _due = datetime.today()
+        if item['due_date_utc']:
+            _due = self.__get_localtime(item['due_date_utc'])
         _sched = PyOrgMode.OrgSchedule()
 
         if 'SCHEDULED' in todo and todo['SCHEDULED']:
-            todo['SCHEDULED'].scheduled = PyOrgMode.OrgDate("<%s>" % _due.strftime("%F"))
+            scheduled = todo['SCHEDULED'].scheduled.get_value()
+            if scheduled[1:11] != _due.strftime("%F"):
+                todo['SCHEDULED'].scheduled = PyOrgMode.OrgDate("<%s>" % _due.strftime("%F"))
+                self.changed = 1
         else:
             _sched._append(todo['node'], _sched.Element(scheduled="<%s>" % _due.strftime("%F")))
+            self.changed = 1
 
     def __process_props(self, todo, item):
         if 'PROPERTIES' in todo:
@@ -69,12 +82,19 @@ class OrgTodoist:
             _props.append(PyOrgMode.OrgDrawer.Property("TODOIST_ID", "%d" % item['id']))
             todo['node'].append_clean(_props)
 
+        self.changed = 1
+
     def bind(self):
+        item_ids = []
         for item in self.todoist_items:
             found = 0
             for todo in self.todos:
                 if 'TODOIST_ID' in todo and int(todo['TODOIST_ID']) == item['id']:
-                    todo['node'].heading = item['content'].encode('utf-8').strip()
+                    item_ids.append(todo['TODOIST_ID'])
+                    heading = item['content'].encode('utf-8').strip()
+                    if todo['node'].heading != heading:
+                        todo['node'].heading = heading
+                        self.changed = 1
 
                     self.__process_scheduled(todo, item)
 
@@ -90,8 +110,10 @@ class OrgTodoist:
 
                 self.org.root.append_clean(new_todo)
 
+                self.changed = 1
+
         for todo in self.todos:
-            if 'TODOIST_ID' in todo and todo['node'].todo == 'DONE':
+            if 'TODOIST_ID' in todo and todo['node'].todo == 'DONE' and todo['TODOIST_ID'] in item_ids:
                 item = self.api.items.get_by_id(int(todo['TODOIST_ID']))
                 item.complete()
                 self.api.commit()
@@ -106,18 +128,15 @@ class OrgTodoist:
 
 
     def save(self):
-        self.org.save_to_file(self.org_file)
+        if self.changed:
+            self.org.save_to_file(self.org_file)
 
 def main():
-    cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
-    sys.path.insert(0, cmd_folder + "dist/PyOrgMode")
-    sys.path.insert(0, cmd_folder + "dist/todoist-python")
-
-    #orgTodoist = OrgTodoist()
-    #orgTodoist.read_todos()
-    #orgTodoist.sync_todoist()
-    #orgTodoist.bind()
-    #orgTodoist.save()
+    orgTodoist = OrgTodoist()
+    orgTodoist.read_todos()
+    orgTodoist.sync_todoist()
+    orgTodoist.bind()
+    orgTodoist.save()
 
 if __name__ == "__main__":
     main()
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/tima/sources/OrgTodoist/
+/usr/bin/python /home/tima/sources/OrgTodoist/main.py
